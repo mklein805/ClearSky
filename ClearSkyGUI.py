ab99b08acb77754e55118bf3e5f8b1ffa817968e
Marissa <93284571+mklein805@users.noreply.github.com>
2022-03-28 08:41:16 -0400

    Changed how windows are structured

--- ClearSkyGUI.py
+++ ClearSkyGUI.py
@@ -11,10 +11,34 @@ from ClearSky import ClearSky
 sg.theme('DarkBlue15')
 
 layout = [
-    [sg.Text('Enter your Location'), sg.InputText()],
-    [sg.Submit(),sg.Cancel()]
+    [sg.Text('Enter your Location',font=('Helvetica',25)), sg.InputText()],
+    [sg.Text('', key = '-TEXT-')],
+    [sg.Submit(),sg.Exit()]
     ]
 
+window = sg.Window('ClearSky',layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    else:
+        loc = values[0]
+        
+        #Gets the forecast
+        sky = ClearSky()
+        weather = sky.getForecast(loc)
+        forecast = "\n\n".join(weather)
+        window['-TEXT-'].update(forecast)
+
+window.close()
+        
+    
+    
+
+
+
+'''
 window = sg.Window('ClearSky', layout)
 event, values = window.read()
 window.close()
@@ -29,3 +53,4 @@ forecast = "\n\n".join(weather)
 
 #Displays forecast
 sg.popup(forecast, title = 'Forecast for '+loc)
+'''
